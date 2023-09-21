cf744d0031c6676b9c631362a4615017c9e55b92
pan <2995332268@qq.com>
2023-09-21 22:43:59 +0800

    刚能与浏览器交互

--- src/main/java/org/example/server/ServerBoot.java
+++ src/main/java/org/example/server/ServerBoot.java
@@ -1,24 +1,46 @@
 package org.example.server;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 public class ServerBoot {
     //引导服务端启动的方法
     public void bootServer(){
+        Logger logger = Logger.getLogger(ServerBoot.class.getName());
         //记得要进行多线程处理,用一个currentHashMap存入session
         while (true){
             //这个端口后面会写成配置文件
-            try (ServerSocket serverSocket = new ServerSocket(8081)) {
+            try (ServerSocket serverSocket = new ServerSocket(8081);) {
+                String htmlLine;
+                StringBuilder html= new StringBuilder();
+                logger.info("服务器启动");
                 Socket accept = serverSocket.accept();
                 InputStream inputStream = accept.getInputStream();
-                byte[] bytes = inputStream.readAllBytes();
-                String message = new String(bytes);
-
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                while (bufferedReader.ready()){
+                    System.out.println(bufferedReader.readLine());
+                }
+                //获取文件
+                FileReader fileReader =
+                        new FileReader("/Users/pan/IdeaProjects/1024competition/src/main/resources/html/index.html");
+                BufferedReader fileBufferReader = new BufferedReader(fileReader);
+                while ((htmlLine=fileBufferReader.readLine()) != null){
+                    html.append(htmlLine);
+                }
+                //响应头
+                String responseHeader = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "Content-Length: " + html.length() + "\r\n"
+                        + "\r\n";
+                OutputStream outputStream = accept.getOutputStream();
+                outputStream.write(responseHeader.getBytes());
+                outputStream.write(html.toString().getBytes());
+                outputStream.flush();
+                accept.close();
             }  catch (IOException e) {
-                e.printStackTrace();
+                logger.warning(e.getMessage());
             }
         }
     }
